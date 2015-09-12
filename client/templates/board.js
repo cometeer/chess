3f8903a8d0f4c70780f232a038ee53cae6f7aaa8
Tristan Geraets <tristang@protonmail.ch>
2015-09-12 11:26:07 +0200

    fixing dumb mistakes and rebuilding working version.

--- client/templates/board.js
+++ client/templates/board.js
@@ -1,6 +1,6 @@
 /* globals Games, Chess */
 Template.board.helpers({
-  rows: function () {
+  rows: function() {
     var id = Session.get('currentGame');
     var game = Games.findOne({
       _id: id
@@ -12,7 +12,7 @@ Template.board.helpers({
     }
     return rows;
   },
-  moveIndicator: function () {
+  moveIndicator: function() {
     var squareNum = this.arrayLoc;
     var indicators = Session.get('moveIndicators');
     var i;
@@ -25,56 +25,46 @@ Template.board.helpers({
     }
     return false;
   },
-  debug: function () {
+  debug: function() {
     console.log(this);
   }
 });
 
 Template.board.events({
-  'click .cell': function (event) {
-    console.log(event.currentTarget.children[0].id);
-    // find the right square on the board
+  'click .cell': function(event) {
     var squareNum = event.currentTarget.children[0].id;
+    var boardId = Session.get('currentGame');
+    var square = Chess.getSquare(boardId, squareNum);
     // if it's already selected clear the selection and quit
     if (squareNum === Session.get('selectedPiece')) {
       Session.set('moveIndicators', undefined);
       Session.set('selectedPiece', undefined);
       return;
     }
-    console.log('not a double click');
     // If an indicated square is clicked we want to move the piece there
     var indicators = Session.get('moveIndicators');
-    var i;
-    var boardId = Session.get('currentGame');
-    var selected = Session.get('selectedPiece');
-    var square = Chess.getSquare(boardId, squareNum);
     if (indicators) {
-      console.log('we\'ve got indicators');
-      console.log(indicators, squareNum);
-      for (i = 0; i <= indicators.length; i++) {
+      var selected = Session.get('selectedPiece');
+      for (var i = 0; i <= indicators.length; i++) {
         if (indicators[i] == squareNum) {
-          console.log('made it this far');
           var p = Chess.getSquare(boardId, selected).piece;
-          console.log(p);
           var movingPiece = Chess.rePie(p.type, p.color);
-          console.log(movingPiece);
-          movingPiece.move(selected, squareNum);
+          movingPiece.move(boardId, p, selected, squareNum);
+          Session.set('moveIndicators', undefined);
+          Session.set('selectedPiece', undefined);
+          return;
         }
       }
     }
-    console.log('not a move');
+    // If a square that has a piece is clicked show tha possible moves
     if (square.piece && square.piece !== {}) {
-      var p = square.piece;
-      square.piece = Chess.rePie(p.type, p.color);
-      console.log(square);
+      square.piece = Chess.rePie(square.piece.type, square.arrayLoc);
       // get the valid moves for this piece
+      console.log('click handler ', square)
       var moveOptions = square.piece.validMoves(square);
-      console.log(moveOptions);
       // For each valid move update the board to show it.
       Session.set('moveIndicators', moveOptions);
       Session.set('selectedPiece', squareNum);
     }
   }
-
 });
-
